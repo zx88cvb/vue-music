300f10950e5af6943fb4c9db739549a6991927cf
zx88cvb <295389754@qq.com>
2018-07-22 20:17:01 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,6 +5,29 @@
 </template>
 
 <script>
+import {getSingerList} from 'api/singer'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (ERR_OK === res.code) {
+          this.singers = res.data.list
+          console.log(this.singers)
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
