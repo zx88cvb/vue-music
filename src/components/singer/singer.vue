15b350a306bf6388ad8bdb2d62779fa91bd1132a
zx88cvb <295389754@qq.com>
2018-07-23 15:25:28 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="singer" ref="singer">
-    歌手页面
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
 import {getSingerList} from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 
 export default {
   data () {
@@ -21,11 +26,61 @@ export default {
     _getSingerList () {
       getSingerList().then((res) => {
         if (ERR_OK === res.code) {
-          this.singers = res.data.list
+          this.singers = this._normalizeSinger(res.data.list)
           console.log(this.singers)
         }
       })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+
+      // 排序
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[0-9a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+
+      return hot.concat(ret)
     }
+  },
+  components: {
+    ListView
   }
 }
 </script>
