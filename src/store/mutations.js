76b76238365126d9bee4ce4f171917b73a9d4693
zx88cvb <295389754@qq.com>
2018-08-09 17:04:50 +0800

    player

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -6,6 +6,24 @@ const mutations = {
     try {
       localStorage.singer = singer
     } catch (e) {}
+  },
+  [types.SET_PLAYING_STATE] (state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN] (state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAYLIST] (state, playlist) {
+    state.playlist = playlist
+  },
+  [types.SET_SEQUENCE_LIST] (state, sequenceList) {
+    state.sequenceList = sequenceList
+  },
+  [types.SET_PLAY_MODE] (state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CURRENT_INDEX] (state, index) {
+    state.currentIndex = index
   }
 }
 
